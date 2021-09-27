8f062b5b25c451a610e327f0ba6e58f880b8aea4
Valeriu Cojocari <valeriu.cojocari@alvys.com>
2021-09-27 16:12:51 +0300

    asdasd

--- src/app/modules/home.module.ts
+++ src/app/modules/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../components/home/home.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { HomeComponent } from '../components/home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class HomeModule { }
