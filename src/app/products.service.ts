94cee5efad02b4335eb7db7118b3cf8891da73e2
Valeriu Cojocari <valeriu.cojocari@alvys.com>
2021-09-27 16:56:14 +0300

    asdasd

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import data from '../assets/products.JSON';
+import data from 'assets/products.json';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductsService {
 
   constructor() { }
