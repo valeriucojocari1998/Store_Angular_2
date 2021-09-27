739427bd86d375a0ae02d52902ccdbaea73b9e08
Valeriu Cojocari <valeriu.cojocari@alvys.com>
2021-09-27 15:01:31 +0300

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutModule } from './modules/about.module';
+import { CartModule } from './modules/cart.module';
+import { HomeModule } from './modules/home.module';
+import { ProductsModule } from './modules/products.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule,
+    AboutModule,
+    CartModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
